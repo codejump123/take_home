1a98a0ebb861b0ea01f4e7ad2d7929591671a6b3
codejump123 <ipokha14@gmail.com>
2022-11-22 02:20:38 -0500

    deltete metric.sql files

--- b_and_b/models/final/metric_total_order_count.sql
+++
@@ -1,6 +0,0 @@
-select * 
-from {{ metrics.calculate(
-    metric('total_order_count'),
-    grain='day',
-    dimensions=['order_created_at', 'product_category','product_title','product_variant_sku','product_variant_style','product_variant_size']
-) }}
